619ec6f79731593c8ff83d49c0b4d84d27b31625
Joaquin <joaquin.solla@udc.es>
2022-06-11 20:14:01 +0200

    Added Event color filter (fix)

--- lib/pages/all_pages.dart
+++ lib/pages/all_pages.dart
@@ -1,7 +1,4 @@
 export 'home.dart';
-export 'home_views/todo_view.dart';
-export 'home_views/settings_view.dart';
-export 'home_views/habits_view.dart';
 export 'events/add_event.dart';
 export 'events/event_details.dart';
 export 'events/edit_event.dart';
