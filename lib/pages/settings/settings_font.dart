77b70f125f81fc70a0b62803ac13ad3aea62448a
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2023-03-09 02:08:20 +0100

    Updated font settings page

--- lib/pages/settings/settings_font.dart
+++ lib/pages/settings/settings_font.dart
@@ -172,6 +172,14 @@ class _SettingsFontState extends State<SettingsFont> {
             FooterCredits(),
           ],),
           [
+            Text('Esto es una previsualización:',
+              style: TextStyle(
+                  color: colorMainText,
+                  fontSize: deviceWidth * previewFontSize * 0.045,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: deviceHeight * 0.005),
+
             Column(
               children: [
                 SizedBox(height: deviceHeight*0.005,),
@@ -225,111 +233,27 @@ class _SettingsFontState extends State<SettingsFont> {
                 SizedBox(height: deviceHeight*0.005,),
               ],
             ),
-            Column(
+
+            SizedBox(height: deviceHeight * 0.025),
+            Text('Ajusta el tamaño del texto a tu gusto, una vez termines pulsa '
+                'el botón de abajo para guardar los cambios.\n\n'
+                'También se actualiza el tamaño de los iconos y botones:',
+              style: TextStyle(
+                color: colorSecondText,
+                fontSize: deviceWidth * previewFontSize * 0.0325,
+                fontWeight: FontWeight.normal),),
+
+            SizedBox(height: deviceHeight * 0.025),
+            Wrap(
+              alignment: WrapAlignment.center,
               children: [
-                SizedBox(height: deviceHeight*0.005,),
-                Container(
-                  padding: EdgeInsets.all(deviceWidth * 0.0185),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(color),
-                  ),
-                  child: IntrinsicHeight(
-                    child: Row(
-                      children: [
-                        Container(
-                          width: deviceWidth*0.175,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text('Tarea', style: TextStyle(color: colorMainText, fontSize: deviceWidth * previewFontSize * 0.055, fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        ),
-                        VerticalDivider(color: colorSecondText,),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                                width: deviceWidth*0.585,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Tarea',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(color: colorMainText, fontSize: deviceWidth * previewFontSize * 0.06, fontWeight: FontWeight.bold))),
-                            SizedBox(height: deviceHeight*0.00375,),
-                            Container(
-                                width: deviceWidth*0.585,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Descripción de la tarea',
-                                    maxLines: 5,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(color: colorSecondText, fontSize: deviceWidth * previewFontSize * 0.03, fontWeight: FontWeight.normal))),
-                          ],
-                        ),
-                        SizedBox(width: deviceWidth*0.01,),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: deviceHeight*0.005,),
-              ],
-            ),
-            Column(
-              children: [
-                SizedBox(height: deviceHeight*0.005,),
-                Container(
-                  padding: EdgeInsets.all(deviceWidth * 0.0185),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(color),
-                  ),
-                  child: IntrinsicHeight(
-                    child: Row(
-                      children: [
-                        Container(
-                          width: deviceWidth*0.175,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text('Nota', style: TextStyle(color: colorMainText, fontSize: deviceWidth * previewFontSize * 0.055, fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        ),
-                        VerticalDivider(color: colorSecondText,),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                                width: deviceWidth*0.585,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Nota',
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(color: colorMainText, fontSize: deviceWidth * previewFontSize * 0.06, fontWeight: FontWeight.bold))),
-                            SizedBox(height: deviceHeight*0.00375,),
-                            Container(
-                                width: deviceWidth*0.585,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Contenido de la nota',
-                                    maxLines: 5,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(color: colorSecondText, fontSize: deviceWidth * previewFontSize * 0.03, fontWeight: FontWeight.normal))),
-                          ],
-                        ),
-                        SizedBox(width: deviceWidth*0.01,),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: deviceHeight*0.005,),
-              ],
-            ),
-            // TODO: add routine
+                Icon(Icons.add, color: colorSecondText, size: deviceWidth*previewFontSize*0.085),
+                SizedBox(width: deviceWidth * 0.075),
+                Icon(Icons.search, color: colorSecondText, size: deviceWidth*previewFontSize*0.085),
+                SizedBox(width: deviceWidth * 0.075),
+                Icon(Icons.clear_all_rounded, color: colorSecondText, size: deviceWidth*previewFontSize*0.085),
+            ],),
+
           ]
       ),
     );
